ebf2b502c4831ef14ca59d9bab63cbf755ba6138
fabioingenito <fabio.ingenito@hotmail.com>
2025-08-25 12:03:06 -0300

    :tada: Curso ReactJS

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import img from '../assets/Cachorro.svg'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Bem-vindo ao Pet Shop do Pedro!</h1>
+      <img src={img} alt="Ilustração de um petshop" />
+      <a href="https://storyset.com/love">Love illustrations by Storyset</a>
+      {/* https://storyset.com/illustration/animal-shelter/bro */}
+    </header>
+  );
+}
+
+//export default Header;
