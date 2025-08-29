d3973917fa57b55da55398728cbfbda0ae513044
fabioingenito <fabio.ingenito@hotmail.com>
2025-08-29 12:03:44 -0300

    ':sparkles: feat: Aula 03

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import Navbar from './components/navbar/Navbar'
+import Carousel from "./components/carousel/Carousel";
+import CategoryCard from "./components/categoryCard/categoryCard";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
+      <Carousel title="Carrosel 1" />
+      <Carousel title="Carrosel 2" /> */}
+      <CategoryCard
+        title={"Brinquedos"}
+        productCount={"84 itens"}
+        image={
+          "https://coelmar.com.br/wp-content/uploads/2024/07/Bolinha-pra-Cachorro.jpg"
+        }
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
